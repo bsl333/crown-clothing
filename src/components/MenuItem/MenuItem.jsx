f35197342657e17715828827d200a9cc764e7e47
Branden <b.lowe333@gmail.com>
2019-11-20 22:59:29 -0800

    styling updates

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './MenuItem.scss';
 
 export default function MenuItem({ title, imageUrl, size }) {
+  const menuItemClass = size ? `${size} menu-item` : 'menu-item';
   return (
-    <div
-      className={`${size} menu-item`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
+    <div className={menuItemClass}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">Shop now</span>
