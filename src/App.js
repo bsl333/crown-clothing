84c5191f694d26da225e3df7c89e08bd721418a9
Branden <b.lowe333@gmail.com>
2019-11-23 13:11:17 -0800

    created header comp and style

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
 
 import './App.scss';
+import Header from './components/Header/Header';
 
 export default function App() {
   return (
     <div className="container">
+      <Header />
       <Route exact path="/" component={HomePage}></Route>
       <Route exact path="/shop" component={ShopPage}></Route>
     </div>
