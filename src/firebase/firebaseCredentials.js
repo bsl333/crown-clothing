c797d212bfb193b5db3bee8bf7a55010a9cb19f0
Branden <b.lowe333@gmail.com>
2019-11-29 12:20:09 -0800

    must expose firebase credentials to deploy

---
+++ src/firebase/firebaseCredentials.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyChoBxcZcEd3u13AqkaQQZBlRMP-HAtQFY',
+  authDomain: 'crown-clothing-db-1b6f7.firebaseapp.com',
+  databaseURL: 'https://crown-clothing-db-1b6f7.firebaseio.com',
+  projectId: 'crown-clothing-db-1b6f7',
+  storageBucket: 'crown-clothing-db-1b6f7.appspot.com',
+  messagingSenderId: '297310187306',
+  appId: '1:297310187306:web:8867bf4f8af4ebca167853',
+  measurementId: 'G-P9SH08NZXK'
+};
